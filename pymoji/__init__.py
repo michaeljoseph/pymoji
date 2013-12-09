d1fd32946ba422e8f240bd44bffab3107f4d1057
Michael Joseph <michaeljoseph+github@gmail.com>
2013-12-09 12:17:36 +0200

    Return the emoji and format it

--- pymoji/__init__.py
+++ pymoji/__init__.py
@@ -1,10 +1,13 @@
-"""Python Library Boilerplate contains all the boilerplate you need to create a Python package."""
+"""Emits HTML from emoji"""
 
 __author__ = 'Michael Joseph'
 __email__ = 'michaeljoseph@gmail.com'
 __url__ = 'https://github.com/michaeljoseph/pymoji'
 __version__ = '0.0.1'
 
+from .emoji import emoji
 
-def pymoji():
-    return 'Hello World!'
+def pymoji(text):
+	if text[0] <> text[:-1] and text[0] <> ':':
+		text = ':%s:' % text
+	return emoji(text)
